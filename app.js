a0db2cacd02c91e3c4a2d0224c0f05737088abab
da <dain940209@gmail.com>
2023-12-03 19:06:47 +0900

    ranking cr

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import rankRouter from './routes/rank.router.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3017;
 
 app.use(express.json());
+// // 특정 출처만 허용하도록 CORS 미들웨어 설정
+// const corsOptions = {
+//   origin: 'http://localhost:5173', // 원하는 출처를 여기에 지정
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true,
+//   optionsSuccessStatus: 204,
+// };
+
+app.use(cors());
 app.use('/api', [rankRouter]);
 
 app.listen(PORT, () => {
--- routes/rank.router.js
+++ routes/rank.router.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { prisma } from '../utils/prisma/index.js';
 const router = express.Router();
 
-// 게시글 생성
+// 스코어 등록
 router.post('/ranking', async (req, res, next) => {
   const { name, score } = req.body;
   const rank = await prisma.ranking.create({
@@ -11,11 +11,33 @@ router.post('/ranking', async (req, res, next) => {
       score : score
     },
   });
-
   return res.status(201).json({ data: rank });
+
+  // // 랭킹 반환
+  // const ranking = await prisma.ranking.findMany({
+  //   select: {
+  //     rankId: true,
+  //     name: true,
+  //     score: true,
+  //     createdAt: true,
+  //     updatedAt: true,
+  //   },
+  //   orderBy: {
+  //     score: 'desc', // 'desc'는 내림차순, 'asc'는 오름차순
+  //   },
+  // });
+
+  // // 찾은 랭킹에서 새로 추가한 랭크의 인덱스(순위)를 찾습니다.
+  // const newIndex = ranking.findIndex(item => item.rankId === rank.rankId);
+
+  // // rank 배열에 인덱스 추가
+  // const rankedWithIndex = ranking.map((item, index) => ({ ...item, rank: index + 1 }));
+  
+  // // 새로 추가한 랭크에 대한 정보를 출력합니다.
+  // console.log(rankedWithIndex[newIndex]);
 });
 
-/** 게시글 전체 조회 API **/
+/** 랭킹 목록 조회 API **/
 router.get('/ranking', async (req, res, next) => {
   const rank = await prisma.ranking.findMany({
     select: {
@@ -24,9 +46,16 @@ router.get('/ranking', async (req, res, next) => {
       createdAt: true,
       updatedAt: true,
     },
+    orderBy: {
+      score: 'desc', // 'desc'는 내림차순, 'asc'는 오름차순
+    },
   });
+  // rank 배열에 인덱스 추가
+  const rankedWithIndex = rank.map((item, index) => ({ ...item, rank: index + 1 }));
 
-  return res.status(200).json({ data: rank });
+  // console.log(rankedWithIndex);
+
+  return res.status(200).json({ data: rankedWithIndex });
 });
 
 export default router;
